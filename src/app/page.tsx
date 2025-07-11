6813cedcce99e6c4913dc96f7821b59b35496b86
Jeroen van de Sanden <vandesanden@hotmail.com>
2025-07-11 10:40:17 +0200

    feat: update homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
-"use client"
-
-import { Container } from "./_features/test.styles";
+import Link from 'next/link'
+import { Button } from '@/components/ui/button'
 
 export default function Home() {
   return (
-    <Container><h1>Test</h1><p>This is a test paragraph.</p></Container>
-  );
+    <div className="w-screen h-screen bg-black flex justify-center items-center text-white">
+      <div>
+        <h1 className="text-4xl pb-2">Fitness Tracker</h1>
+        <p className="italic text-white/65 text-lg">
+          Be strong. Be big. Track progress.
+        </p>
+        <div className="mt-4">
+          <Button
+            asChild
+            className="bg-blue-700 capitalize w-full text-lg py-6"
+          >
+            <Link href="/training">Get started</Link>
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
